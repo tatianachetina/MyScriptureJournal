8f50dd22910d43255e167f78c7d755e5b438f58b
tatianachetina <tanya.chetina23@gmail.com>
2019-06-26 16:42:44 -0600

    Initial Commit

---
+++ Models/Scripture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+        public string Book { get; set; }
+        public int Chapter { get; set; }
+        public int Verse { get; set; }
+
+        [Display(Name = "Added Date")]
+        [DataType(DataType.Date)]
+        public DateTime AddDate { get; set; }
+        public string Note { get; set; }
+        //public string Price { get; set; }
+    }
+}
---
+++ Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Models.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Models.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Books { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string ScriptureBook { get; set; }
+
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
+        {
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            //CurrentFilter = SearchString;
+            // Use LINQ to get list of genres.
+            IQueryable<string> bookQuery = from s in _context.Scripture
+                                            orderby s.Book
+                                            select s.Book;
+            var scriptures = from s in _context.Scripture
+                             select s;
+            //var notes = from s in _context.Scripture
+                             //select s;
+
+            switch (sortOrder)
+            {
+                case "book":
+                    scriptures = scriptures.OrderBy(s => s.Book);
+                    break;
+                case "date":
+                    scriptures = scriptures.OrderBy(s => s.AddDate);
+                    break;
+               
+                default:
+                    scriptures = scriptures.OrderBy(s => s.Book);
+                    break;
+            }
+
+            
+
+            //if (!string.IsNullOrEmpty(SearchString))
+            //{
+                //scriptures = scriptures.Where(s => s.Book.Contains(SearchString));
+            //}
+
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                scriptures = scriptures.Where(s => s.Note.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(ScriptureBook))
+            {
+                scriptures = scriptures.Where(x => x.Book == ScriptureBook);
+            }
+            Books = new SelectList(await bookQuery.Distinct().ToListAsync());
+
+            Scripture = await scriptures.ToListAsync();
+            //Scripture = await notes.ToListAsync();
+        }
+    }
+}
